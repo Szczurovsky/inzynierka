dafc9b1c0c07836adab0aa68b9341bd2bca1f981
Dawid Szczurkowski <dszczurkowski@gmail.com>
2022-04-03 16:42:24 +0200

    test

--- src/components/Characters/Characters.jsx
+++ src/components/Characters/Characters.jsx
@@ -36,6 +36,7 @@ export const Characters = props => {
             <p>sssss</p>
             {/* {console.log(props)} */}
             {console.log(props.aktstat)}
+            {cosnole.log(props)}
             <button onClick={()=>props.saveStats({siła:3, sila_woli:5})}>sss</button> 
          
     
