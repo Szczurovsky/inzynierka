000f6aff51c4bd505d284acda7571f4defcb9e93
Dawid Szczurkowski <dszczurkowski@gmail.com>
2022-05-22 20:12:27 +0200

    moze zepsuc v2

--- src/App.js
+++ src/App.js
@@ -54,6 +54,7 @@ function App(props) {
                 handleLogin={handleLogin}
                 setUser={setUser}
                 setLoginStatus={setLoginStatus}
+                loginStatus={loginStatus}
             />
         </div>
     );
