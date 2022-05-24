9638197a88eaadf2f4478ffb1d278dcb75e32dd2
Dawid Szczurkowski <dszczurkowski@gmail.com>
2022-05-24 22:51:06 +0200

    menu change colors,add transition

--- src/components/Menu/MenuPC/MenuPC.jsx
+++ src/components/Menu/MenuPC/MenuPC.jsx
@@ -20,14 +20,18 @@ const Button = styled(Link)`
     width:100%;
     height:20%;
     text-decoration:none;
-  background: rgb(10,33,149);
-background: radial-gradient(circle, rgba(10,33,149,0.5466561624649859) 0%, rgba(28,4,9,0.16290266106442575) 100%);
+
+    background: radial-gradient(circle, rgba(0,0,0,0.5466561624649859) 30%, rgba(255,255,255,0.16290266106442575) 200%);
     color:white;
     display:flex;align-items:center;
     justify-content:center;
     border-bottom:1px solid black;
     border-top:1px solid black;
-
+      &:hover{
+            transition:0.5s;
+    /*      background: radial-gradient(circle, rgba(0,0,0,0.5466561624649859) 30%, rgba(255,255,255,0.16290266106442575) 200%); */
+            color:red;
+  }
 `
 export const MenuPC = props => {
     return (
